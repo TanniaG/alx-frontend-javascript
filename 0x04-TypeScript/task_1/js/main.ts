02bede979373b4cd72e7a86bd3349b9e87733655
TanniaG <tannia.gachoki@strathmore.edu>
2025-10-18 16:25:55 +0300

    task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+// Task 1: Teacher Interface
+interface Teacher {
+  readonly firstName: string;        // Can only be set at creation
+  readonly lastName: string;         // Can only be set at creation
+  fullTimeEmployee: boolean;         // Must always be provided
+  yearsOfExperience?: number;        // Optional (the ? makes it optional)
+  location: string;                  // Must always be provided
+  [key: string]: any;                // Index signature - allows any additional properties
+}
+
+// Example usage
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,                   // This is allowed because of [key: string]: any
+  yearsOfExperience: 5,              // Optional, but we're providing it
+};
+
+console.log(teacher3);
