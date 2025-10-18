03c0e5a840a08f082864a913e6b71244f030eb3e
TanniaG <tannia.gachoki@strathmore.edu>
2025-10-18 16:41:59 +0300

    task 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,19 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// Task 2: Directors interface extending Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example usage
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
