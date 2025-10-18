d7328ea28e5617278470a83552c84fcbee51bedf
TanniaG <tannia.gachoki@strathmore.edu>
2025-10-18 22:25:29 +0300

    declare function

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,9 +1,7 @@
 // Task 8.2: Ambient declarations for crud.js
 import { RowID, RowElement } from './interface';
 
-declare module './crud' {
-  export function insertRow(row: RowElement): RowID;
-  export function deleteRow(rowId: RowID): void;
-  export function updateRow(rowId: RowID, row: RowElement): RowID;
-}
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
 
